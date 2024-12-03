203989519b0e7436a2744b8480e67687cff3ab98
sudhanshu2070 <sssallen2016@gmail.com>
2024-12-04 01:00:49 +0530

    Navbar 0.2

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,40 +6,118 @@ const Navbar: React.FC = () => {
   const location = useLocation();
 
   // Don't show the navbar on login and signup pages
-  if (location.pathname === "/" || location.pathname === "/signup") {
+  const hideNavbar = location.pathname === "/" || location.pathname === "/signup";
+
+  // Add/remove the blur class dynamically
+  const applyBlur = (shouldBlur: boolean) => {
+    let divToBlur: HTMLElement | null = null; // Declare divToBlur once
+
+    if (location.pathname === "/home") {
+      divToBlur = document.querySelector('.home-container');
+    } else if (location.pathname === "/add-contact") {
+      divToBlur = document.querySelector('.page-background'); // Add Contact
+    } else if (location.pathname === "/contact") {
+      divToBlur = document.querySelector('.slider-container'); // View Contact
+    }
+    else{
+      return false;
+    }
+    
+    if (divToBlur !== null) {
+      if (shouldBlur) {
+        divToBlur.classList.add('blur');
+      } else {
+        divToBlur.classList.remove('blur');
+      }
+    }
+  };
+
+  // Function to show or hide the menu
+  const toggleMenuVisibility = (isVisible: boolean) => {
+    const menu = document.getElementsByClassName("navbar-menu")[0] as HTMLElement;
+    if (menu) {
+      if(isVisible){
+        menu.style.display = "block";
+      }
+      else{
+        menu.style.display = "none";
+      }
+    }
+  };
+
+  // If on login or signup page, return null early to not render navbar
+  if (hideNavbar) {
     return null;
   }
 
   return (
     <div className="navbar-container">
-      <div className="navbar-button">
-        {/* Hamburger Icon */}
-        <div className="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
+      {/* Hamburger Icon */}
+      <div className="hamburger"  id="divHamburger" onMouseEnter={() => { toggleMenuVisibility(true)}}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Navbar Menu */}
+      <div className="navbar-menu"
+        onMouseEnter={() => { applyBlur(true)}} // Apply blur when mouse enters
+        onMouseLeave={() => {
+          toggleMenuVisibility(false); // Close menu
+          applyBlur(false); // Remove blur when mouse leaves
+        }}
+      >
+        {/* Close Button */}
+        <div
+            className="close-button"
+              onClick={() => {
+                toggleMenuVisibility(false); // Close the menu
+                applyBlur(false); // Remove the blur
+              }}
+          >
+          &times;
         </div>
-        {/* Navbar Menu */}
-        <div className="navbar-menu">
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/add-contact">Add Contact</Link>
-            </li>
-            <li>
-              <Link to="/contact">View Contacts</Link>
-            </li>
-            <li>
-              <Link to="/cool-stuff">Cool Stuff</Link>
-            </li>
-            <li>
-              <Link to="/">Log Out</Link>
-            </li>
-          </ul>
-          <div className="navbar-footer">Keep Smiling 😊</div>
-        </div>
+
+        {/* Menu Items */}
+        <ul>
+          <li>
+          <Link to="/home" onClick={() => {
+            toggleMenuVisibility(false) ;
+            applyBlur(false); // Remove blur when mouse leaves
+          }}>Home</Link>
+          </li>
+          <li>
+            <Link to="/add-contact" onClick={() => {
+              toggleMenuVisibility(false) ;
+              applyBlur(false); // Remove blur when mouse leaves
+            }}
+            >Add Contact</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => {
+              toggleMenuVisibility(false) ;
+              applyBlur(false); // Remove blur when mouse leaves
+            }}
+            >View Contacts</Link>
+          </li>
+          <li>
+            <Link to="/cool-stuff" onClick={() => {
+              toggleMenuVisibility(false) ;
+              applyBlur(false); // Remove blur when mouse leaves
+            }}
+            >Cool Stuff</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={() => {
+              toggleMenuVisibility(false) ;
+              applyBlur(false); // Remove blur when mouse leaves
+            }}
+            >Log Out</Link>
+          </li>
+        </ul>
+
+        {/* Footer */}
+        <div className="navbar-footer">Keep Smiling 😊</div>
       </div>
     </div>
   );
